55a7485126c627f5830d16d838475d7895aa9ac3
wrunion <winterrunion@gmail.com>
2020-09-05 14:06:17 -0700

    Print out search results.

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,43 +3,37 @@ import axios from 'axios'
 
 const Search = () => {
   const [term, setTerm] = useState('programming');
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
+    if (term) {
     const search = async () => {
-      await axios.get('https://en.wikipedia.org/w/api.php', {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
         params: {
           action: 'query', 
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term,
+          srsearch: term
         },
       });
+      setResults(data.query.search);
     };
 
     search();
+    }
   }, [term]);
-  // useEffect(() => {
-  //   const search = async () => {
-  //     await axios.get('https://www.example.org/w/api.php'), {
-  //       params: {
-  //         action: 'query',
-  //         list: 'search',
-  //         origin: '*',
-  //         format: 'json',
-  //         srsearch: term
-  //       },
-  //     };
-  //   };
 
-  //   search();
-  // }, [term]);
-
-  // const search = () => {
-  //   axios.get('https://www.example.org/w/api.php'), {
-
-  //   }
-  // }
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
+        </div>
+      </div>
+    )
+  })
 
   return (
     <div>
@@ -51,6 +45,9 @@ const Search = () => {
             onChange={e => setTerm(e.target.value)}
             className="input" />
         </div>
+      </div>
+      <div className="ui celled list">
+        {renderedResults}
       </div>
     </div>
   )
